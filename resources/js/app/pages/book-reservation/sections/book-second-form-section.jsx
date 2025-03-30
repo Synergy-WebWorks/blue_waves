a8fbc85456f5a7b47e6584ace9d7589269037e20
QuicklyDG <quicklydeguzman@gmail.com>
2025-03-30 10:28:27 +0800

    added placeholder for address

--- resources/js/app/pages/book-reservation/sections/book-second-form-section.jsx
+++ resources/js/app/pages/book-reservation/sections/book-second-form-section.jsx
@@ -285,6 +285,7 @@ export default function BookSecondFormSection() {
                                             }
                                             type="text"
                                             autoComplete="street-address"
+                                            placeholder="(e.g., 56 Lopez Jaena St, Brgy. 7, Bacolod City, Negros Occidental, 6100, Philippines)"
                                             className="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-600 sm:text-sm/6"
                                             required
                                         />
